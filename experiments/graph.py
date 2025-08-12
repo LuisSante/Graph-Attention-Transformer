f29574616a86c78b6a300f9116058a5343f3ea35
LuisSante <lsantet@unsa.edu.pe>
2025-08-12 17:27:23 -0300

    feat: 🎸 advances

--- experiments/graph.py
+++ experiments/graph.py
@@ -1,9 +1,7 @@
 import numpy as np
 import config
-from  utils.utils import init_weights, leaky_relu, softmax_with_mask
-from data.dataset import create_graph, adj_to_edge_index, edge_index_to_adj
-from models.gat_layer import GATLayer
-
+from data.dataset import create_graph
+from models.gat_layer import MultiHeadGATLayer
 
 if __name__ == "__main__":
     adj, features, labels = create_graph(num_nodes=6, num_features=5, num_classes=3, self_loops=True)
@@ -11,15 +9,16 @@ if __name__ == "__main__":
     print("Features shape:", features.shape)
     print("Labels:", labels)
 
-    gat_layer = GATLayer(in_features=features.shape[1], out_features=5, seed=config.SEED)
-    h_out, alpha = gat_layer.forward(features, adj, return_attention=True)
+    multihead_gat = MultiHeadGATLayer(in_features=features.shape[1], out_features_per_head=5, n_heads=3, concat=True, seed=config.SEED)
+
+    # Forward
+    h_out, alphas = multihead_gat.forward(features, adj, return_attentions=True)
 
     print("\nOutput features (h') shape:", h_out.shape)
     print(h_out)
 
-    print("\nAttention coefficients (alpha) shape:", alpha.shape)
-    print(alpha[:3, :3])
-
-    row_sums = np.sum(alpha, axis=1)
-    print("\nRow sums (deben ser 1.0 en nodos con vecinos):", row_sums)
+    for idx, alpha in enumerate(alphas):
+        print(f"\nHead {idx} alpha (3x3 sample):\n", alpha[:3, :3])
+        row_sums = np.sum(alpha, axis=1)
+        print("Row sums (deben ser 1.0 en nodos con vecinos):", row_sums)
 
